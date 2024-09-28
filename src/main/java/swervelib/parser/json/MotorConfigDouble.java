6e38b271ee16fa4b03e97d48b981bdb5d4010c79
Ayush Kumar <github@ayukmr.com>
2024-09-27 17:38:34 -0700

    Add Prettier

--- src/main/java/swervelib/parser/json/MotorConfigDouble.java
+++ src/main/java/swervelib/parser/json/MotorConfigDouble.java
@@ -3,34 +3,30 @@ package swervelib.parser.json;
 /**
  * Used to store doubles for motor configuration.
  */
-public class MotorConfigDouble
-{
+public class MotorConfigDouble {
 
-  /**
-   * Drive motor.
-   */
-  public double drive;
-  /**
-   * Angle motor.
-   */
-  public double angle;
+    /**
+     * Drive motor.
+     */
+    public double drive;
+    /**
+     * Angle motor.
+     */
+    public double angle;
 
-  /**
-   * Default constructor.
-   */
-  public MotorConfigDouble()
-  {
-  }
+    /**
+     * Default constructor.
+     */
+    public MotorConfigDouble() {}
 
-  /**
-   * Default constructor.
-   *
-   * @param angle Angle data.
-   * @param drive Drive data.
-   */
-  public MotorConfigDouble(double angle, double drive)
-  {
-    this.angle = angle;
-    this.drive = drive;
-  }
+    /**
+     * Default constructor.
+     *
+     * @param angle Angle data.
+     * @param drive Drive data.
+     */
+    public MotorConfigDouble(double angle, double drive) {
+        this.angle = angle;
+        this.drive = drive;
+    }
 }
--- src/main/java/swervelib/parser/json/PIDFPropertiesJson.java
+++ src/main/java/swervelib/parser/json/PIDFPropertiesJson.java
@@ -5,15 +5,14 @@ import swervelib.parser.PIDFConfig;
 /**
  * {@link swervelib.SwerveModule} PID with Feedforward for the drive motor and angle motor.
  */
-public class PIDFPropertiesJson
-{
+public class PIDFPropertiesJson {
 
-  /**
-   * The PIDF with Integral Zone used for the drive motor.
-   */
-  public PIDFConfig drive;
-  /**
-   * The PIDF with Integral Zone used for the angle motor.
-   */
-  public PIDFConfig angle;
+    /**
+     * The PIDF with Integral Zone used for the drive motor.
+     */
+    public PIDFConfig drive;
+    /**
+     * The PIDF with Integral Zone used for the angle motor.
+     */
+    public PIDFConfig angle;
 }
