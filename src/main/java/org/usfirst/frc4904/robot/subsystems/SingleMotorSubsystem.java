2f0df9673f216502d1b40e0487d6b8cc7f3e9c4b
Sam Matteson <sammatt@nuevaschool.org>
2025-01-10 17:18:37 -0800

    remove duplicate code in SingleMotorSubsystem

--- src/main/java/org/usfirst/frc4904/robot/subsystems/SingleMotorSubsystem.java
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/SingleMotorSubsystem.java
@@ -2,29 +2,30 @@ package org.usfirst.frc4904.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class SingleMotorSubsystem extends SubsystemBase{
+public class SingleMotorSubsystem extends SubsystemBase {
+
     public final CANTalonFX motor;
-    public SingleMotorSubsystem(CANTalonFX motor){
+
+    public SingleMotorSubsystem(CANTalonFX motor) {
         this.motor = motor;
         this.motor.setIdleMode(IdleMode.kBrake);
+    }
 
+    public Command c_setVoltage(int voltage) {
+        return this.run(() -> {
+                this.motor.setVoltage(voltage);
+            });
     }
-    public Command c_forward(){
-        var cmd = this.run(() -> {
-            this.motor.setVoltage(1);
-        });
-        return cmd;
+
+    public Command c_forward() {
+        return this.c_setVoltage(1);
     }
-    public Command c_backward(){
-        var cmd = this.run(() -> {
-            this.motor.setVoltage(-1);
-        });
-        return cmd;
+
+    public Command c_backward() {
+        return this.c_setVoltage(-1);
     }
-    public Command c_stop(){
-        var cmd = this.run(() -> {
-            this.motor.setVoltage(0);
-        });
-        return cmd;
+
+    public Command c_stop() {
+        return this.c_setVoltage(0);
     }
 }
